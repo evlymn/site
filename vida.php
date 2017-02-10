57e377a517b5655e523c1c26fbaf998bf35aa5df
jeliel <jelielmendes@gmail.com>
2017-02-10 18:46:51 -0200

    Novas entradaa em vida e troca de imagem de insta

---
+++ vida.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!-- saved from url=(0039)http://kamsolutions.pl/projects/resume/ -->
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trans Nerd</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="Css/style.css">
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <script src="https://use.fontawesome.com/9ca832a819.js"></script>
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+
+        </div>
+      </div>
+    </div>
+  </body>
